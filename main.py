b56330ca37bffbc9c7103fa568f39b4d30015fd7
Luis David Florez Pareja <luisflorexpareja@gmail.com>
2024-11-17 21:26:33 -0500

    Add simple WebSocket

--- main.py
+++ main.py
@@ -1,27 +1,12 @@
-from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import StreamingResponse, HTMLResponse
+from fastapi import FastAPI, File, UploadFile, WebSocket
+from fastapi.responses import HTMLResponse
 import cv2
 import math
 import ultralytics
-import nest_asyncio
-from pyngrok import ngrok
 import uvicorn
-import os
+import asyncio
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
-
-model = ultralytics.YOLO("yolo11l.pt")
-
-# Configuración del texto y los colores
-font = cv2.FONT_HERSHEY_SIMPLEX
-fontScale = 0.7
-box_color = (0, 0, 255)  # Color del bounding box
-text_color = (255, 255, 255)  # Color del texto
-bg_color = (0, 0, 0)  # Color del fondo del texto
-box_thickness = 2
+model = ultralytics.YOLO("yolo11n.pt")
 
 app = FastAPI()
 
@@ -29,7 +14,7 @@ app = FastAPI()
 temp_video_path = "uploaded_video.mp4"
 
 
-def video_frame_generator():
+async def video_frame_generator(websocket: WebSocket):
     # Utilizar el video subido para el procesamiento
     cap = cv2.VideoCapture(temp_video_path)
     while cap.isOpened():
@@ -47,49 +32,22 @@ def video_frame_generator():
 
                 # Calcular la confianza y el texto de la clase
                 confidence = math.ceil((box.conf[0] * 100))
-                label = f"{r.names[int(box.cls[0])]} {confidence}%"
+                label = r.names[int(box.cls[0])]
 
                 # Imprimir resultados
                 print(
-                    f"LABEL: {r.names[int(box.cls[0])]}  COORDINATES: {(x1, y1)}, { (x2, y2)}  CONFIDENCE: {confidence}%"
-                )
-
-                # Dibujar el bounding box con un grosor más grueso
-                cv2.rectangle(frame, (x1, y1), (x2, y2), box_color, box_thickness)
-
-                # Medidas del texto
-                (text_width, text_height), baseline = cv2.getTextSize(
-                    label, font, fontScale, box_thickness
-                )
-
-                # Fondo del texto
-                cv2.rectangle(
-                    frame,
-                    (x1, y1 - text_height - 10),
-                    (x1 + text_width + 4, y1),
-                    bg_color,
-                    -1,
-                )
-
-                # Colocar el texto encima del fondo
-                cv2.putText(
-                    frame,
-                    label,
-                    (x1 + 2, y1 - 5),
-                    font,
-                    fontScale,
-                    text_color,
-                    thickness=2,
+                    f"LABEL: {label}  COORDINATES: {(x1, y1)}, { (x2, y2)}  CONFIDENCE: {confidence}%"
                 )
 
         # Codificar el frame a JPEG
-        _, buffer = cv2.imencode(".jpg", frame)
+        _, buffer = cv2.imencode(".jpg", results[0].plot())
         frame_bytes = buffer.tobytes()
 
-        # Formato para streaming de imágenes
-        yield (
-            b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame_bytes + b"\r\n"
-        )
+        # Enviar el frame como datos binarios a través del WebSocket
+        await websocket.send_bytes(frame_bytes)
+
+        # Añadir un pequeño retraso para evitar saturar al cliente
+        await asyncio.sleep(0.03)
 
     cap.release()
 
@@ -101,11 +59,11 @@ async def upload_video(file: UploadFile = File(...)):
     return {"message": "Video uploaded successfully"}
 
 
-@app.get("/stream")
-async def stream_video():
-    return StreamingResponse(
-        video_frame_generator(), media_type="multipart/x-mixed-replace; boundary=frame"
-    )
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    await video_frame_generator(websocket)
+    await websocket.close()
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -114,20 +72,5 @@ async def index():
     return HTMLResponse(content=html_content)
 
 
-# Get your authtoken from https://dashboard.ngrok.com/get-started/your-authtoken
-auth_token = os.getenv("NGROK_TOKEN")
-
-# Set the authtoken
-ngrok.set_auth_token(auth_token)
-
-# Connect to ngrok
-ngrok_tunnel = ngrok.connect(9000)
-
-# Print the public URL
-print("Public URL:", ngrok_tunnel.public_url)
-
-# Apply nest_asyncio
-nest_asyncio.apply()
-
-# Run the uvicorn server
-uvicorn.run(app, port=9000)
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=9000)
